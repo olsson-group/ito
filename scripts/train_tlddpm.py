9b4b55ad3db5690ae831b6ef8380f722d1fb0291
Mathias Schreiner <jamas@dtu.dk>
2023-11-09 15:22:29 +0100

    format arguments and have indistinguishable and unscaled

--- scripts/train_tlddpm.py
+++ scripts/train_tlddpm.py
@@ -1,3 +1,4 @@
+import os
 import json
 from argparse import ArgumentParser
 
@@ -9,7 +10,6 @@ from ito.model import cpainn, ddpm
 
 
 def main(args):
-
     score_model = cpainn.PaiNNTLScore(
         n_features=args.n_features, max_lag=args.max_lag, diff_steps=args.diff_steps
     )
@@ -19,15 +19,17 @@ def main(args):
     )
 
     dataset = data.ALA2Dataset(
-        path="data/ala2",
+        path=os.path.join(args.path, "data/ala2"),
         max_lag=args.max_lag,
-        distinguish=args.distinguish,
+        distinguish=not args.indistinguishable,
         fixed_lag=args.fixed_lag,
+        scale=not args.unscaled,
     )
 
     dataloader = DataLoader(dataset, batch_size=args.batch_size, shuffle=True)
 
     trainer = pl.Trainer(
+        default_root_dir=args.path,
         max_epochs=args.epochs,
         gradient_clip_val=1.0,
     )
@@ -35,22 +37,21 @@ def main(args):
     trainer.fit(model, dataloader)
 
 
-
-
 if __name__ == "__main__":
     parser = ArgumentParser()
 
     # fmt: off
-    parser.add_argument("--n_features",  nargs="?", type=int,   default=64)
-    parser.add_argument("--n_layers",    nargs="?", type=int,   default=2)
-    parser.add_argument("--epochs",      nargs="?", type=int,   default=50)
-    parser.add_argument("--diff_steps",  nargs="?", type=int,   default=1000)
-    parser.add_argument("--batch_size",  nargs="?", type=int,   default=128)
-    parser.add_argument("--lr",          nargs="?", type=float, default=1e-3)
-    parser.add_argument("--max_lag",     nargs="?", type=int,   default=1000)
-    parser.add_argument("--fixed_lag",   action='store_true')
-    parser.add_argument("--distinguish", action='store_true')
-    parser.add_argument("--scale",       action='store_true')
+    parser.add_argument("--path",              nargs="?", type=str,   default=".")
+    parser.add_argument("--n_features",        nargs="?", type=int,   default=64)
+    parser.add_argument("--n_layers",          nargs="?", type=int,   default=2)
+    parser.add_argument("--epochs",            nargs="?", type=int,   default=50)
+    parser.add_argument("--diff_steps",        nargs="?", type=int,   default=1000)
+    parser.add_argument("--batch_size",        nargs="?", type=int,   default=128)
+    parser.add_argument("--lr",                nargs="?", type=float, default=1e-3)
+    parser.add_argument("--max_lag",           nargs="?", type=int,   default=1000)
+    parser.add_argument("--fixed_lag",         action='store_true')
+    parser.add_argument("--indistinguishable", action='store_true')
+    parser.add_argument("--unscaled",          action='store_true')
     # fmt: on
 
     main(parser.parse_args())
