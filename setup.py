95c8e2f722d05372f18df4dc3c2499ae4da548ef
Mathias Schreiner <jamas@dtu.dk>
2023-11-09 12:06:40 +0100

    add setup.py

---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import find_packages, setup
+
+
+setup(
+    name='ito',
+    packages=find_packages(),
+    install_requires=[
+        'torch',
+        'pytorch_lightning',
+        'torch_geometric',
+    ],
+)
